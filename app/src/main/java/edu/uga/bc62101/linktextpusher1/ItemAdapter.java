441e6e486ded58d37de1157b905e367ce110d19a
Benson Chau <bchau3440@gmail.com>
2016-10-17 23:36:03 -0400

    completed listview custom adapter

--- app/src/main/java/edu/uga/bc62101/linktextpusher1/ItemAdapter.java
+++ app/src/main/java/edu/uga/bc62101/linktextpusher1/ItemAdapter.java
@@ -1,9 +1,13 @@
 package edu.uga.bc62101.linktextpusher1;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -19,7 +23,32 @@ public class ItemAdapter extends ArrayAdapter<Item> {
     @Override
     public View getView( int position, View convertView, ViewGroup parent ){
         View v = convertView;
+        if( v == null ){
+            LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(R.layout.list_item, null);
+        }
 
+        Item item = items.get(position);
+
+        if( item != null ){
+            TextView content = (TextView) v.findViewById(R.id.content);
+            TextView username = (TextView) v.findViewById(R.id.username);
+            TextView source = (TextView) v.findViewById(R.id.source);
+
+            if( username != null ){
+                username.setText(item.getUserID());
+            }
+
+            if( content != null ){
+                content.setText(item.getContent());
+            }
+
+            if( source != null ){
+                source.setText(item.getSource());
+            }
+        }
+
+        return v;
     }
 
 }
